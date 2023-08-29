965aa018fdb3fc70f751ed2cda1131bbfdf5f4a3
litovkaa <nikita.litovka@lenta.com>
2023-08-29 17:32:14 +0300

    https://leetcode.com/problems/generate-parentheses/description/

---
+++ generate_parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generateParenthesis(self, n: int, s: str = "(") -> List[str]:
+        o = len([el for el in s if el == "("])
+        c = len([el for el in s if el == ")"])
+        if o == n and c == n:
+            return [s]
+
+        if o >= c:
+            if o < n:
+                return self.generateParenthesis(n, s=s+"(") + self.generateParenthesis(n, s=s+")")
+            else:
+                return self.generateParenthesis(n, s=s+")")
+        else:
+            return []
